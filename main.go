ececcabddfcda4a9dbf85af651e3ae01765e7f40
Alex M <amarcoci@skyslope.com>
2020-04-17 15:32:15 -0700

    Get user input after the first question

--- main.go
+++ main.go
@@ -25,6 +25,9 @@ func main() {
 	originUnit = strings.ToUpper(os.Args[1])
 
 	for {
+
+		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+
 		_, err = fmt.Scanln(&originValue)
 		if err != nil {
 			printError(errReadingInput)
@@ -34,8 +37,6 @@ func main() {
 		} else {
 			convertToCelsius(originValue)
 		}
-
-		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 
